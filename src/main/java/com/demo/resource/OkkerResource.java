8a1d5fccab270752f83dc0d81441bd98a785fb06
Robert Geczi <v-rgeczi@hotels.com>
2017-03-01 17:39:32 +0100

    logging

--- src/main/java/com/demo/resource/OkkerResource.java
+++ src/main/java/com/demo/resource/OkkerResource.java
@@ -1,5 +1,9 @@
 package com.demo.resource;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OkkerResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OkkerResource.class);
 
     @RequestMapping(value = "/webhook", method = {RequestMethod.GET, RequestMethod.POST})
-    public ResponseEntity<Void> webhook() {
+    public ResponseEntity<Void> webhook(HttpServletRequest request) {
+        log(request.getRemoteAddr());
         return ResponseEntity.ok().build();
+    }
+
+    private void log(String remoteAddr) {
+        LOGGER.info("Visit from '{}'.", remoteAddr);
     }
 }
